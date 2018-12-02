a58cce73c9b6bb952da7821803b701258f979167
retosen <34863491+retosen@users.noreply.github.com>
2018-12-02 19:56:14 +0000

    Fixing download links

--- src/_data/downloads.js
+++ src/_data/downloads.js
@@ -7,28 +7,28 @@ export const buttons = {
   "osx": {
     icon: faApple,
     desc: "MacOS",
-    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/Garlicoin-x86_64-apple-darwin11.tar.gz`
+    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/garlicoin-0.17.2-osx64.tar.gz`
   },
   "windows64": {
     icon: faWindows,
     desc: "Windows (64-bit)",
-    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/Garlicoin-x86_64-w64-mingw32.tar.gz`
+    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/garlicoin-0.17.2-win64.zip`
   },
   "windows": {
     icon: faWindows,
     desc: "Windows (32-bit)",
-    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/Garlicoin-i686-w64-mingw32.tar.gz`
+    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/garlicoin-0.17.2-win32.zip`
   },
   "linux": {
     icon: faLinux,
     desc: "Linux",
-    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/Garlicoin-x86_64-unknown-linux-gnu.tar.gz`
+    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/garlicoin-0.17.2-x86_64-linux-gnu.tar.gz`
   },
   "RPI": {
     icon: faMicrochip,
     rotate: 90,
     desc: "Raspberry Pi",
-    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/Garlicoin-arm-linux-gnueabihf.tar.gz`
+    link: `https://github.com/GarlicoinOrg/Garlicoin/releases/download/${release}/garlicoin-0.17.2-arm-linux-gnueabihf.tar.gz`
   },
   "github": {
     icon: faGithub,
